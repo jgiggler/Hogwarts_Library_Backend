fd435162dabeab486874960c257c29c683a49da6
Joel Gilger <gilgerj@flip2.engr.oregonstate.edu>
2024-02-27 21:47:21 -0800

    created folders for statis and templates added routes

--- app.py
+++ app.py
@@ -18,18 +18,71 @@ mysql = MySQL(app)
 # Routes
 @app.route('/')
 def root():
-    
 
-    query4 = 'SELECT * FROM Books;';
+    return render_template("index.j2")
+
+@app.route("/books", methods=["POST", "GET"])
+def books():
+    # Separate out the request methods, in this case this is for a POST
+    # insert a person into the bsg_people entity
+    if request.method == "POST":
+        # fire off if user presses the Add Book button
+        if request.form.get("addBook"):
+            # grab user form inputs
+            isbn = request.form["isbn"]
+            title = request.form["title"]
+            genre = request.form["genre"]
+            copies = request.form["copies"]
+            available = request.form["available"]
+            price = request.form["price"]
+
+            # account for null genre
+            if genre == "":
+                # mySQL query to insert a new person into bsg_people with our form inputs
+                query = "INSERT INTO Books (isbn, title, copies, available, price) VALUES (%s, %s, %s, %s, %s)"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (isbn, title, copies, available, price))
+                mysql.connection.commit()
+
+            # no null inputs
+            else:
+                query = "INSERT INTO Books (isbn, title, genre, copies, available, price) VALUES (%s, %s, %s, %s, %s, %s)"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (isbn, title, genre, copies, available, price))
+                mysql.connection.commit()
+
+            # redirect back to people page
+            return redirect("/books")
+
+    # Grab bsg_people data so we send it to our template to display
+    if request.method == "GET":
+        # mySQL query to grab all the people in bsg_people
+        query = "SELECT bookISBN, bookTitle, bookGenre, copyTotal, copyAvailable, bookCost FROM Books GROUP BY bookGenre ORDER BY bookTitle;"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        # render edit_people page passing our query data and homeworld data to the edit_people template
+        return render_template("books.j2", data=data)
+
+
+# route for delete functionality, deleting a person from bsg_people,
+# we want to pass the 'id' value of that person on button click (see HTML) via the route
+@app.route("/delete_people/<int:id>")
+def delete_people(id):
+    # mySQL query to delete the person with our passed id
+    query = "DELETE FROM bsg_people WHERE id = '%s';"
     cur = mysql.connection.cursor()
-    cur.execute(query4)
-    results = cur.fetchall()
+    cur.execute(query, (id,))
+    mysql.connection.commit()
 
-    return "<h1>MySQL Results</h1>" + str(results[0])
+    # redirect back to people page
+    return redirect("/people")
+
 
 
 # Listener
 if __name__ == "__main__":
 
     #Start the app on port 3000, it will be different once hosted
-    app.run(port=4545, debug=True)
+    app.run(port=4546, debug=True)
