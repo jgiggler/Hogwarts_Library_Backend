46052524559c3e4cf5f5a12e922fa21eb23c32c6
Rimsha Ejaz <ejazr@flip4.engr.oregonstate.edu>
2024-03-18 07:12:03 -0700

    null for edit res

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ import os
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
-app.config['MYSQL_USER'] = 'cs340_ejazr'
-app.config['MYSQL_PASSWORD'] = '4524' #last 4 of onid
-app.config['MYSQL_DB'] = 'cs340_ejazr'
+app.config['MYSQL_USER'] = 'cs340_ONID'
+app.config['MYSQL_PASSWORD'] = 'XXXX' #last 4 of onid
+app.config['MYSQL_DB'] = 'cs340_ONID'
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 
